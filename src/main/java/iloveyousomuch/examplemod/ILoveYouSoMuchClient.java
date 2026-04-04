3fa6d38cc6e342c581ce31aa6f197444893f370c
Ethan Pezzulo <ethan.pezzulo@gmail.com>
2026-04-04 01:52:56 -0700

    Update to include working kissing code

--- src/main/java/iloveyousomuch/examplemod/ILoveYouSoMuchClient.java
+++ src/main/java/iloveyousomuch/examplemod/ILoveYouSoMuchClient.java
@@ -1,5 +1,6 @@
 package iloveyousomuch.examplemod;
 
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -7,19 +8,25 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.minecraft.network.protocol.common.ServerboundCustomPayloadPacket;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+
+import com.mojang.blaze3d.platform.InputConstants;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = ILoveYouSoMuch.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
 @EventBusSubscriber(modid = ILoveYouSoMuch.MODID, value = Dist.CLIENT)
 public class ILoveYouSoMuchClient {
+    // Keybind for kissing
+    public static final KeyMapping KISS_KEY = new KeyMapping("key.iloveyousomuch.kiss", InputConstants.KEY_R, "key.categories.multiplayer");
+
     public ILoveYouSoMuchClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
-        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+        // container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
     @SubscribeEvent
@@ -27,5 +34,18 @@ public class ILoveYouSoMuchClient {
         // Some client setup code
         ILoveYouSoMuch.LOGGER.info("HELLO FROM CLIENT SETUP");
         ILoveYouSoMuch.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+
+    @SubscribeEvent
+    public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
+        event.register(KISS_KEY);
+    }
+
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Pre event) {
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.player != null && mc.player.connection != null && KISS_KEY.consumeClick()) {
+            mc.player.connection.send(new ServerboundCustomPayloadPacket(new KissMessage()));
+        }
     }
 }
